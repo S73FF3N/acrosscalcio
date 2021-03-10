dd9d290f01e390d2bfe095cbea2c3093d74c145c
Steffen Schroedter <List@MacBook-Air.fritz.box>
2021-03-10 22:25:17 +0100

    added interated navigation

--- allstar_teams/urls.py
+++ allstar_teams/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 #router.register(r'countries', views.CountriesViewSet)
 
 urlpatterns = [
-    url(r'^$', views.AllstarTeamView.as_view()),
+    url(r'^$', views.AllstarTeamView.as_view(), name='club_allstar_team_list'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.allstar_team_detail, name='allstar_team_detail'),
     url(r'^player/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.allstar_player_detail, name='allstar_player_detail'),
     url(r'^send_mail_to_all_illustrators/$', views.send_mail_to_all_illustrators, name='email'),
